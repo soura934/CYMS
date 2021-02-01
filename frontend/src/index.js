2beb8d65ce682c449df5fa28aced3273d3038817
soobin <nhsb00@hotmail.com>
2021-02-01 10:01:54 -0500

    create index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Root from './components/root';
+import configureStore from './store/store';
+import jwt_decode from 'jwt-decode';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import { setAuthToken } from './util/session_api_util';
+import { logout } from './actions/session_actions';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+document.addEventListener('DOMContentLoaded', () => {
+  let store;
+
+  if (localStorage.jwtToken) {
+    setAuthToken(localStorage,jwtToken);
+
+    const decoded = jwt_decode(localStorage.jwtToken);
+    const preloadedState = { session: { isAuthenticated: true, user: decoded }};
+
+    store = configureStore(preloadedState);
+
+    const currentTime = Date.now() / 1000;
+
+    if ( decoded.exp < currentTime ) {
+      store.dispatch(logout());
+      
+    }
+  } else {
+    store = configureStore({});
+  }
+  const root = document.getElementById('root');
+  ReactDOM.render(<Root store={ store }/>, root);
+});
