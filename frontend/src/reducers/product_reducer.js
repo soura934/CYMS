4507df27763cf1bbad0a5c0467d462caf63e9bf9
cthorne <cfthorne83@gmail.com>
2021-02-01 23:02:08 -0500

    add products action/reducer

---
+++ frontend/src/reducers/product_reducer.js
@@ -0,0 +1,24 @@
+import {
+    RECEIVE_ALL_PRODUCTS,
+    RECEIVE_PRODUCT,
+    REMOVE_PRODUCT,
+} from '../actions/board_actions';
+
+const productsReducer = (state = {}, action) => {
+    Object.freeze(state);
+
+    switch (action.type) {
+        case RECEIVE_ALL_PRODUCTS:
+            return action.products;
+        case RECEIVE_PRODUCT:
+            return Object.assign({}, state, { [action.product.id]: action.product });
+        case REMOVE_PRODUCT:
+            let newState = Object.assign({}, state);
+            delete newState[action.productId];
+            return newState;
+        default:
+            return state;
+    }
+}
+
+export default productsReducer;
