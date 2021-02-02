0511f3b984491f1c4921828eb364693c78a65db1
soobin <nhsb00@hotmail.com>
2021-02-02 11:15:43 -0500

    create products models, routes, and validation

---
+++ models/Product.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ProductSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Product = mongoose.model('product', ProductSchema);
---
+++ validation/products.js
@@ -0,0 +1,35 @@
+const Validator = require('validator');
+const validText = require('./valid-text');
+
+module.exports = function validateProductInput(data) {
+  let errors = {};
+
+  data.title = validText(data.title) ? data.title : '';
+  data.description = validText(data.description) ? data.description : '';
+  data.price = validText(data.price) ? data.price : '';
+
+  if (!Validator.isLength(data.title, { min: 0, max: 100 })) {
+    errors.title = 'Product Title must be less than 100 characters';
+  }
+
+  if (Validator.isEmpty(data.title)) {
+    errors.title = 'Title field is required';
+  }
+
+  if (!Validator.isLength(data.description, { min: 0, max: 255 })) {
+    errors.description = 'Product Description must be less than 255 characters';
+  }
+
+  if (Validator.isEmpty(data.description)) {
+    errors.description = 'Description field is required';
+  }
+
+  if (Validator.isEmpty(data.price)) {
+    errors.price = 'Price field is required';
+  }
+
+  return {
+    errors,
+    isValid: Object.keys(errors).length === 0
+  };
+};
