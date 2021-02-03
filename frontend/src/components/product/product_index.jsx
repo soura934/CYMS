6d34612958ac9322b0b8be6718aa670c6217414e
cthorne <cfthorne83@gmail.com>
2021-02-03 10:10:07 -0500

    add product show link

--- frontend/src/components/product/product_index.jsx
+++ frontend/src/components/product/product_index.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
+
 import ProductIndexItem from './product_index_item'
 import '../../stylesheets/product-index.css';
 
@@ -11,10 +13,12 @@ class ProductIndex extends React.Component {
     render() {
         if (!this.props.products) return null;
 
-        let products = this.props.products.map((product) => {
+        let products = this.props.products.map((product,) => {
             return (
                     <ul className="product-item">
-                        <li>{product.title}</li>
+                        <li>
+                            <Link to={`/${product._id}`}>{product.title}</Link>
+                        </li>
                         {/* <img src="/assets/adjustable desktop.png" alt=""/> */}
                         <li>${product.price}</li>
                         {/* <li>{product.description}</li> */}
