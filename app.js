1b10e28e8200202ee40dc242a3f446ebfdd912e4
Yehudah <jros987@gmail.com>
2021-02-03 15:55:53 -0500

    put back to backend

--- app.js
+++ app.js
@@ -7,7 +7,16 @@ const bodyParser = require('body-parser');
 const users = require("./routes/api/users");
 const User = require('./models/User');
 const products = require('./routes/api/products');
+const path = require('path');
 
+
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,21 +1,12 @@
 import React from 'react';
 import { AuthRoute } from '../util/route_util';
 import { Switch } from 'react-router-dom';
-const path = require('path');
-
-
 import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
 import Footer from './footer/footer';
 import Splash from './splash/splash';
 import ProductShow from './product/product_show'
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('frontend/build'));
-  app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-  })
-}
 const App = () => (
   <div>
    
