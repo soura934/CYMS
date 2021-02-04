66877ed05104c9f12fad730728909aa7e731c1e8
soura934 <soura934@gmail.com>
2021-02-04 13:59:08 -0500

    deleted unnecesary file

--- validation/cart.js
+++
@@ -1,8 +0,0 @@
-// const Validator = require('validator');
-// const validText = require('./valid-text');
-
-// module.exports = function validateCart(data) {
-//     let errors = {};
-
-//     data.text = validText(data.)
-// }
