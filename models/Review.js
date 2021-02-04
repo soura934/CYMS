93690bef32768692dd2a6bb3c9c4ac2a4bc8293c
soura934 <soura934@gmail.com>
2021-02-04 16:17:31 -0500

    commented out review

---
+++ models/Review.js
@@ -0,0 +1,20 @@
+// const mongoose = require('mongoose')
+// const Schema = mongoose.Schema;
+
+// const ReviewSchema = new Schema({
+//     user: {
+//         type: Schema.Types.ObjectId,
+//         ref: 'users'
+//     },
+//     text: {
+//         type: String,
+//         required: true
+//     },
+//     date: {
+//         type: Date,
+//         default: Date.now
+//     }
+// });
+
+// const Review = mongoose.model('reviews', ReviewSchema);
+// module.exports = Review;
---
+++ validation/reviews.js
@@ -0,0 +1,13 @@
+// const Validator = require('validator');
+// const validText = require('./valid-text');
+
+// module.exports = function validateReviewInput(data) {
+//     let errors = {};
+
+//     data.text = validText(data.text) ? data.text : '';
+
+//     if(!Validator.isLength(data.text, {min: 2, max: 200})) {
+//         errors.text = 'Text must be at least bet 2 to 200 characters'
+//     }
+
+// }
