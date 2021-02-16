eeb05c6004acd7bc58c6480c91cf74d4efcf4886
cthorne <cfthorne83@gmail.com>
2021-02-16 17:16:41 -0500

    finish footer styling

--- frontend/src/components/product/product_index.jsx
+++ frontend/src/components/product/product_index.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
-
-// import ProductIndexItem from './product_index_item'
 import '../../stylesheets/product-index.css';
 
 class ProductIndex extends React.Component {
@@ -16,12 +14,10 @@ class ProductIndex extends React.Component {
 
         let products = this.props.products.map((product) => {
             return (
-                    <Link className='product-index-link' key={product._id}  to={`/product/${product.id}/${product._id}`}>
-                        <ul className='product-item' >
-                            <li>{product.title}</li>
-                            <li><img src={product.image} alt=""/></li>
-                            <li>${product.price}</li>
-                        </ul>
+                    <Link className='product-item' key={product._id}  to={`/product/${product.id}/${product._id}`}>
+                        <li>{product.title}</li>
+                        <li><img src={product.image} alt=""/></li>
+                        <li>${product.price}</li>
                     </Link>
             )
         })
