2b4a83071f40f30340ba88fc3d57989e8647c28e
Yehudah <jros987@gmail.com>
2021-02-23 10:27:03 -0500

    merge

--- frontend/src/reducers/carts_reducer.js
+++ frontend/src/reducers/carts_reducer.js
@@ -2,11 +2,12 @@ import{ADD_CART, RECEIVE_CART, REMOVE_FROM_CART} from '../actions/cart_actions';
  
 const cartReducer = (state = [], action) => {
     Object.freeze(state);
-    let nextState = Object.assign([], state.entities.cart);
+    // let nextState = Object.assign([], state.entities.cart);
     switch (action.type) {
         case ADD_CART:
             debugger
-            nextState.push([action.cart.data]);
+            let nextState = Object.assign([], state.cart, {[action.cart.data]:action.cart.data});
+           
            return nextState;
 
         case RECEIVE_CART:
