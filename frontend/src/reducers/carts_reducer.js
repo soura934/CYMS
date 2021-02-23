694d8579373c2088250a407d34606792b5bb082c
soobin <nhsb00@hotmail.com>
2021-02-23 10:24:51 -0500

    delete extra cart file

--- frontend/src/reducers/carts_reducer.js
+++ frontend/src/reducers/carts_reducer.js
@@ -2,7 +2,7 @@ import{ADD_CART, RECEIVE_CART, REMOVE_FROM_CART} from '../actions/cart_actions';
  
 const cartReducer = (state = [], action) => {
     Object.freeze(state);
-    let nextState = Object.assign([], state.entities.cart);
+    let nextState = Object.assign([], state);
     switch (action.type) {
         case ADD_CART:
             debugger
