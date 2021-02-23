802acf3f80cb822ad3e81c6f1be9736acbe8d7a2
cthorne <cfthorne83@gmail.com>
2021-02-23 18:03:25 -0500

    style landing heading

--- frontend/src/components/landing/landing.jsx
+++ frontend/src/components/landing/landing.jsx
@@ -19,11 +19,12 @@ class Landing extends React.Component {
                 <div className='heading'>
                     <h1>Welcome to CYMS.</h1>
                     <h2>Your one-stop-shop for all things related to your software engineering needs.</h2>
-                </div>
 
-                <div>
-                    <strong>We have the gear Software Engineers need to keep creating.</strong>
-                    <img src="https://images.unsplash.com/photo-1461749280684-dccba630e2f6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" alt=""/>
+                    <div className='container'>
+                        <h1 className='centered'>We have the gear Software Engineers need to keep creating.</h1>
+                        <button>Learn More</button>
+                        <img src="https://images.unsplash.com/photo-1461749280684-dccba630e2f6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" alt=""/>
+                    </div>
                 </div>
 
 
