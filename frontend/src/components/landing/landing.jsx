eb6483f063dc65bb1cd73bceab4f148c909a3963
cthorne <cfthorne83@gmail.com>
2021-02-28 18:41:56 -0500

    d

--- frontend/src/components/landing/landing.jsx
+++ frontend/src/components/landing/landing.jsx
@@ -30,7 +30,7 @@ class Landing extends React.Component {
                         <img src="https://images.unsplash.com/photo-1461749280684-dccba630e2f6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" alt=""/>
                         <div className='centered'>
                             <h1>We have the gear Software Engineers need to keep creating.</h1>
-                            <a href="#link">Learn More</a>
+                            {/* <a href="#link">Learn More</a> */}
                             {/* <Link to='/#link'>learn more</Link> */}
                         </div>
                     </div>
