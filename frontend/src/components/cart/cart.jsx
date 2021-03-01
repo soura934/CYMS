a043b7ef14db843e205f1eb54bda39cc8c37976c
cthorne <cfthorne83@gmail.com>
2021-03-01 11:35:19 -0500

    bold dollar sign cart

--- frontend/src/components/cart/cart.jsx
+++ frontend/src/components/cart/cart.jsx
@@ -78,7 +78,7 @@ class Cart extends React.Component{
 
                         <div className="cart-info">
                             {/* <p> <span className="cart-number">{totalnumber}</span> in cart</p> */}
-                            <p>Total price({totalnumber} in cart): $<span className="cart-number">{totalprice}</span></p>
+                            <p>Total price({totalnumber} in cart): <span className="cart-number">${totalprice}</span></p>
                             <button className="checkout-button" onClick={() => this.checkout()}>Check Out</button>
                         </div>
                         {/* <div>
